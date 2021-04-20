ababe6f8d0ae2ada3c5d986b1926122aea788b44
wanglechuan-gif <wanglechuan@evocolabs.com>
2021-04-20 11:18:48 +0800

    add population data

--- src/App.js
+++ src/App.js
@@ -1,79 +1,49 @@
 import Globe from 'react-globe.gl';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import * as d3 from 'd3';
 import covidStats from './covidStats.json'
-import world_population from './datasets/world_population.csv'
 var cors = require('cors')
 
 function App() {
 
   const World = () => {
-  //   const globeEl = useRef();
-  //   const N = 300;
-  //   const gData = [...Array(N).keys()].map(() => ({
-  //     lat: (Math.random() - 0.5) * 180,
-  //     lng: (Math.random() - 0.5) * 360,
-  //     size: Math.random() / 3,
-  //     color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-  //   }));
-  //   console.log(gData)
+    const [countries, setCountries] = useState({ features: []});
+    const [hoverD, setHoverD] = useState();
 
+    useEffect(() => {
+      // load data
+      fetch('./ne_110m_admin_0_countries.geojson').then(res => res.json()).then(setCountries);
+    }, []);
 
-  //   useEffect(() => {
-  //     // Auto-rotate
-  //     globeEl.current.controls().autoRotate = true;
-  //     globeEl.current.controls().autoRotateSpeed = 0.1;
-  //   }, []);
+    const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
 
-  // return (
-  //   <Globe
-  //     ref={globeEl}
-  //     globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-  //     bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-  //     backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+    // GDP per capita (avoiding countries with small pop)
+    const getVal = feat => feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
 
-  //     pointsData={gData}
-  //     pointAltitude="size"
-  //     pointColor="color"
-  //   />
-  // )
-  // };
+    const maxVal = useMemo(
+      () => Math.max(...countries.features.map(getVal)),
+      [countries]
+    );
+    colorScale.domain([0, maxVal]);
 
-  const globeEl = useRef();
-  const [popData, setPopData] = useState([]);
+    return <Globe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      lineHoverPrecision={0}
 
-  useEffect(() => {
-    // load data
-    fetch('https://github.com/vasturiano/react-globe.gl/blob/master/example/datasets/world_population.csv')
-      .then(res => res.text())
-      .then(csv => d3.csvParse(csv, ({ lat, lng, pop }) => ({ lat: +lat, lng: +lng, pop: +pop })))
-      .then(setPopData);
-  }, []);
-
-  useEffect(() => {
-    // Auto-rotate
-    globeEl.current.controls().autoRotate = true;
-    globeEl.current.controls().autoRotateSpeed = 0.1;
-  }, []);
-
-  const weightColor = d3.scaleSequentialSqrt(d3.interpolateYlOrRd)
-    .domain([0, 1e7]);
-
-  return <Globe
-    ref={globeEl}
-    globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-    bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-    backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-
-    hexBinPointsData={world_population}
-    hexBinPointWeight="pop"
-    hexAltitude={d => d.sumWeight * 6e-8}
-    hexBinResolution={4}
-    hexTopColor={d => weightColor(d.sumWeight)}
-    hexSideColor={d => weightColor(d.sumWeight)}
-    hexBinMerge={true}
-    enablePointerInteraction={false}
-  />;
+      polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
+      polygonAltitude={d => d === hoverD ? 0.12 : 0.06}
+      polygonCapColor={d => d === hoverD ? 'steelblue' : colorScale(getVal(d))}
+      polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+      polygonStrokeColor={() => '#111'}
+      polygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+        GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+        Population: <i>${d.POP_EST}</i>
+      `}
+      onPolygonHover={setHoverD}
+      polygonsTransitionDuration={300}
+    />;
   };
 
   return (
